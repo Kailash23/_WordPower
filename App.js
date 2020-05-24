173487fd4a3a0c971b7b3b3c75335fee69826e93
Kailash Uniyal <kailashuniyal04@gmail.com>
2020-05-24 22:23:50 +0530

    project created

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Text>My Vocabulary</Text>
+      </>
+    );
+  }
+}
+
+export default App;
