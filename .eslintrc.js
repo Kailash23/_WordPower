0033b9f5712f83314e22fb5fd46364d4ee9fecb0
Kailash Uniyal <kailashuniyal04@gmail.com>
2020-05-24 23:29:12 +0530

    project structure added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        alias: {
+          _assets: './src/assets',
+          _components: './src/components',
+          _atoms: './src/components/atoms',
+          _scenes: './src/scenes',
+          _services: './src/services',
+          _styles: './src/styles',
+          _utils: './src/utils',
+        },
+      },
+    },
+  },
 };
+
+// To avoid lint errors when using the new alias
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+const App = () => (
+  <View>
+    <Text>Hello World</Text>
+    <Text style={{fontFamily : "OpenSans-Bold"}}>Hello World</Text>
+  </View>
+);
+
+export default App;
